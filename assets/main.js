38c135656594df3be93cb8dfa1b769bfcd0202f9
Santiago Madariaga <santiagoamadariaga@gmail.com>
2022-10-05 22:05:24 -0300

    Finalizados el JS y los estilos

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,6 @@ const title = document.getElementById("title");
 const namePoke = document.getElementById("name_poke");
 const imgPoke = document.getElementById("img_poke");
 const typePoke = document.getElementById("type_poke");
-const typeTwoPoke = document.getElementById("type_two_poke");
 const weightPoke = document.getElementById("weight_poke");
 const heightPoke = document.getElementById("height_poke");
 
@@ -13,9 +12,7 @@ const fetchPokemons = async (input) => {
   try {
     const res = await fetch(`${baseURL}/${input}`);
     const data = await res.json();
-    renderPokes(data);
-    return data;
-    // console.log(data);
+    return renderPokes(data);
   } catch (error) {
     renderError();
     title.textContent = `No existe ese Pókemon en el registro.`;
@@ -31,13 +28,19 @@ const renderError = () => {
   namePoke.classList.add(`none`);
   imgPoke.parentElement.classList.add(`none`);
   typePoke.classList.add(`none`);
-  typeTwoPoke.classList.add(`none`);
   weightPoke.classList.add(`none`);
   heightPoke.classList.add(`none`);
 };
 
 const renderPokes = (dataPokes) => {
   const { name, types, height, weight, sprites } = dataPokes;
+
+  const typesPokes = () => {
+    return types
+      .map((tipo) => tipo.type.name)
+      .join(" - ")
+      .toUpperCase();
+  };
 
   title.classList.remove(`title_init`);
   title.classList.add(`none`);
@@ -47,14 +50,12 @@ const renderPokes = (dataPokes) => {
   namePoke.classList.remove(`none`);
   imgPoke.parentElement.classList.remove(`none`);
   typePoke.classList.remove(`none`);
-  typeTwoPoke.classList.remove(`none`);
   weightPoke.classList.remove(`none`);
   heightPoke.classList.remove(`none`);
 
-  namePoke.textContent = name;
+  namePoke.textContent = name.toUpperCase();
   imgPoke.setAttribute("src", sprites.other.home.front_default);
-  typePoke.textContent = types[0].type.name;
-  typeTwoPoke.textContent = types[1].type.name;
+  typePoke.textContent = typesPokes();
   weightPoke.textContent = `${weight / 10} Kg`;
   heightPoke.textContent = `${height / 10} m`;
 };
